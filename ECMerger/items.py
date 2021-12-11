28671904b9b4e69fc83a55ac14c4eae3b850c0c9
Chase Hampton <chase.hampton10@gmail.com>
2021-12-10 21:46:56 -0600

    Mostly complete lead record collection

--- ECMerger/items.py
+++ ECMerger/items.py
@@ -3,10 +3,14 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
-
-class EcmergerItem(scrapy.Item):
+class EcmergerItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    policy = Field()
+    case_number = Field()
+    member_state = Field()
+    last_decision_date = Field()
+    title = Field()
     pass
--- ECMerger/spiders/ECMerger.py
+++ ECMerger/spiders/ECMerger.py
@@ -3,72 +3,91 @@ import sqlite3
 
 class Merger(scrapy.Spider):
     name = 'Merger'
-    conn = sqlite3.connect("leads.db")
+
+    def __init(self, reset=False, **kwargs):
+        self.reset = reset
+        super().__init__(**kwargs)
+
+    url = 'https://ec.europa.eu/competition/elojade/isef/index.cfm?fuseaction=dsp_result&policy_area_id=1'
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/83.0.4103.116 Safari/537.36"}
+    form_data = {
+        'list_region': '',
+        'new_list_region': '',
+        'policy_area_id': '2',
+        'case_number': '',
+        'case_title': '',
+        'decision_date_from': '',
+        'decision_date_to': '',
+        'PICKUP_nace_code_myLabel': '',
+        'nace_code': '',
+        'nace_code_id': '',
+        'webpub_date_from': '',
+        'webpub_date_to': '',
+        'label_gber': '',
+        'includegberobj_id': '',
+        'notification_date_from': '',
+        'notification_date_to': '',
+        'notification_date': '0_fromToPair',
+        'council_reg_2': '1',
+        'council_reg_1': '1',
+        'simple_proc_yes': '1',
+        'simple_proc_no': '1',
+        'ojpub_date': '0_fromToPair',
+        'ojpub_date_from': '',
+        'ojpub_date_to': '',
+        'merg_not_date': '0_fromToPair',
+        'merg_not_date_from': '',
+        'merg_not_date_to': '',
+        'merg_deadline_date': '0_fromToPair',
+        'merg_deadline_date_from': '',
+        'merg_deadline_date_to': '',
+        'doc_title': '',
+        'antitrust': '1',
+        'cartel': '1',
+        'at_case_min': '',
+        'at_case_max': '',
+        'at_doc_date': '0_fromToPair',
+        'at_doc_date_from': '',
+        'at_doc_date_to': ''
+    }
 
     def start_requests(self):
-        url = 'https://ec.europa.eu/competition/elojade/isef/index.cfm?fuseaction=dsp_result&policy_area_id=1'
-        headers = {
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/83.0.4103.116 Safari/537.36"}
-        form_data = {
-            'list_region': '',
-            'new_list_region': '',
-            'policy_area_id': '2',
-            'case_number': '',
-            'case_title': '',
-            'decision_date_from': '',
-            'decision_date_to': '',
-            'PICKUP_nace_code_myLabel': '',
-            'nace_code': '',
-            'nace_code_id': '',
-            'webpub_date_from': '',
-            'webpub_date_to': '',
-            'label_gber': '',
-            'includegberobj_id': '',
-            'notification_date_from': '',
-            'notification_date_to': '',
-            'notification_date': '0_fromToPair',
-            'council_reg_2': '1',
-            'council_reg_1': '1',
-            'simple_proc_yes': '1',
-            'simple_proc_no': '1',
-            'ojpub_date': '0_fromToPair',
-            'ojpub_date_from': '',
-            'ojpub_date_to': '',
-            'merg_not_date': '0_fromToPair',
-            'merg_not_date_from': '',
-            'merg_not_date_to': '',
-            'merg_deadline_date': '0_fromToPair',
-            'merg_deadline_date_from': '',
-            'merg_deadline_date_to': '',
-            'doc_title': '',
-            'antitrust': '1',
-            'cartel': '1',
-            'at_case_min': '',
-            'at_case_max': '',
-            'at_doc_date': '0_fromToPair',
-            'at_doc_date_from': '',
-            'at_doc_date_to': ''
-        }
+        conn = self.db_conn()
+        if self.reset:
+            self.drop_table(conn)
+        self.create_table(conn)
 
         yield scrapy.FormRequest(
-            url=url,
-            headers=headers,
-            formdata=form_data,
+            url=self.url,
+            headers=self.headers,
+            formdata=self.form_data,
             callback=self.parse
         )
 
     def parse(self, response):
         conn = self.db_conn()
-        self.create_table(conn)
-
-        page = \
-            response.xpath('//table[contains(concat(" ", normalize-space(@class), " "), " list ")]//tr')
-        extracts = []
-        for row in page[::2]:
-            extracts.append(row.xpath('td').extract())
-        page_rows = page.xpath('tr').extract()
-        print(extracts)
+        if response.xpath('//input[@value="Next"]') is not None:
+            page = response.css('.list').xpath('tr[not(descendant::td[contains(@id, "test")])]')
+            from_row = response.xpath('//input[@value="Next"]/@onclick').re(r"\d{2,4}")
+            self.form_data['fromrow'] = from_row
+            self.form_data['fuseaction'] = 'dsp_result'
+            self.form_data['sort'] = 'case_code asc'
+            for row in page:
+                case = row.css('.case')
+                case_no = case.xpath('./span/text()').get()
+                url = response.urljoin(case.xpath('./a/@href').get())
+                dec_date = \
+                    row.css('.decision').xpath('./text()').get().replace('\r', '').replace('\n', '').replace('\t', '')
+                title = row.css('.title').xpath('./text()').get().replace('\r', '').replace('\n', '').replace('\t', '')
+                self.insert_record(conn, [url, case_no, dec_date, title])
+            return scrapy.FormRequest(
+                url=response.url,
+                formdata=self.form_data,
+                headers=self.headers,
+                callback=self.parse
+            )
 
     def db_conn(self):
         try:
@@ -87,5 +106,11 @@ class Merger(scrapy.Spider):
     def drop_table(self, conn):
         curs = conn.cursor()
         curs.execute("DROP TABLE IF EXISTS records")
+        conn.commit()
+        curs.close()
+
+    def insert_record(self, conn, record):
+        curs = conn.cursor()
+        curs.execute("INSERT INTO records(url, case_no, decision, title) VALUES (?, ?, ?, ?)", record)
         conn.commit()
         curs.close()
